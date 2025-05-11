ef1e612ff712da33ad43b0ebe82333720729e958
ManYak404 <ilya.vaschillo@gmail.com>
2025-05-11 14:37:42 -0700

    new gun assets

--- CrimeWave/Assets/Scripts/Gun.cs
+++ CrimeWave/Assets/Scripts/Gun.cs
@@ -11,8 +11,8 @@ public class Gun : ScriptableObject
     [SerializeField] public float range; // Range of the gun in units
     [SerializeField] public float bulletSpeed; // Speed of the bullet in units per second
     [SerializeField] public float bulletSplashDamageRange = 0.5f; // Splash damage range of bullet on hit or impact
-    [SerializeField] public float bulletsPerShot; // Number of bullets fired per shot
-    [SerializeField] public float bulletSpread; // Spread of the bullets in degrees
+    [SerializeField] public float bulletsPerShot = 1; // Number of bullets fired per shot
+    [SerializeField] public float bulletSpread = 0; // Spread of the bullets in degrees
     [SerializeField] public float reloadTime; // Time taken to reload the gun in seconds
     [SerializeField] public int magazineSize; // Number of bullets in the magazine
     [SerializeField] public Sprite gunSprite; // Sprite representing the gun
